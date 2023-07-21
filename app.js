6e6122f49a62eafe560c5e42d5310e0493fab779
Qaiser Waheed <106738666+QaiserWaheed@users.noreply.github.com>
2023-07-21 19:45:04 +0500

    client app enabled

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ app.use("/api", require("./router/fetchData"));
 app.use(require("./router/userRoute"));
 app.use(require("./router/postOrder"));
 
-// Serve static files
-// app.use(express.static(path.join(__dirname, "./super-mart/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./super-mart/build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -35,7 +34,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Internal Server Error" });
-})
+});
 
 
 

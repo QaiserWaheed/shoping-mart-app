7e18757b85cf50f7c6c798812734f9f75b42cf9a
Qaiser Waheed <106738666+QaiserWaheed@users.noreply.github.com>
2023-07-24 17:39:44 +0500

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -40,13 +40,13 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 12);
-    this.confirmPassword = await bcrypt.hash(this.password, 12);
-  }
-  next();
-});
+// userSchema.pre("save", async function (next) {
+//   if (this.isModified("password")) {
+//     this.password = await bcrypt.hash(this.password, 12);
+//     this.confirmPassword = await bcrypt.hash(this.password, 12);
+//   }
+//   next();
+// });
 
 //generating token
 // userSchema.methods.generateAuthToken = async function () {
